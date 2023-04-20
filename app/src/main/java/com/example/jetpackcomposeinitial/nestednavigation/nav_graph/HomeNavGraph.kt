1924121809b4f4a817399309662faf9d26962742
SUDIP-PC\Sudip Mukherjee <mukherjeesudip72@outlook.com>
2023-04-20 13:20:45 +0530

    Nested Routing - Modifications

--- app/src/main/java/com/example/jetpackcomposeinitial/nestednavigation/nav_graph/HomeNavGraph.kt
+++ app/src/main/java/com/example/jetpackcomposeinitial/nestednavigation/nav_graph/HomeNavGraph.kt
@@ -3,7 +3,15 @@ package com.example.jetpackcomposeinitial.nestednavigation
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
+import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import androidx.navigation.navigation
+import com.example.jetpackcomposeinitial.navigation.DETAILS_ARG_ID_KEY
+import com.example.jetpackcomposeinitial.navigation.DETAILS_ARG_NAME_KEY
+import com.example.jetpackcomposeinitial.navigation.DetailsScreen
+import com.example.jetpackcomposeinitial.navigation.HOME_GRAPH_ROUTE
+import com.example.jetpackcomposeinitial.navigation.HomeScreen
+import com.example.jetpackcomposeinitial.navigation.Screen
 
 fun NavGraphBuilder.homeNavGraph(
     navController: NavHostController
@@ -18,19 +26,19 @@ fun NavGraphBuilder.homeNavGraph(
             HomeScreen(navController = navController)
         }
         composable(
-            route = Screen.Detail.route,
+            route = Screen.Details.route,
             arguments = listOf(
-                navArgument(DETAIL_ARGUMENT_KEY) {
+                navArgument(DETAILS_ARG_ID_KEY) {
                     type = NavType.IntType
                     defaultValue = 0
                 },
-                navArgument(DETAIL_ARGUMENT_KEY2) {
+                navArgument(DETAILS_ARG_NAME_KEY) {
                     type = NavType.StringType
                     defaultValue = "Stevdza-San"
                 }
             )
         ) {
-            DetailScreen(navController = navController)
+            DetailsScreen(navController = navController)
         }
     }
 }
